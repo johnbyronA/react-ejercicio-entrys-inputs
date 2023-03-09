7f6d115f10186413a68b2971413ec1881aff642f
John Byron <113152853+johnbyronA@users.noreply.github.com>
2023-03-09 11:39:13 -0500

    Agregado un estado para manejar los entrys de usuario y age

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,20 +4,30 @@ import classes from "./AddUsers.module.css"
 import Button from '../UI/Button';
 
 const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
 
    const addUserHandler = (event) => {
     event.preventDefault();
    } 
+
+   const usernameChangeHandler = (event) => {
+      setEnteredUsername(event.target.value);
+   }
+
+   const ageChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+ }
 
   return (
     <Card className={classes.input}>
       <form onSubmit= {addUserHandler}>
 
           <label htmlFor='username'>Username</label>
-          <input id="username" type="text" />
+          <input id="username" type="text" onChange={usernameChangeHandler} />
 
           <label htmlFor='username'>Age (years)</label>
-          <input id="username" type="number" />
+          <input id="username" type="number" onChange={ageChangeHandler} />
           
           <Button type='submit'>Add User</Button>
 
