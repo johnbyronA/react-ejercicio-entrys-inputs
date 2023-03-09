0f5f7ccd41c08ffb757d8b2281373dbe28b88e18
John Byron <113152853+johnbyronA@users.noreply.github.com>
2023-03-09 14:34:34 -0500

    Agregado un componente UserLists

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import AddUser from './components/Users/AddUser'
+import UsersList from './components/Users/UsersList'
 import "./index.css"
+
 
 const App = () => {
   return (
     <div>
       <AddUser />
+      <UsersList users= {[]} />
     </div>
   )
 }
